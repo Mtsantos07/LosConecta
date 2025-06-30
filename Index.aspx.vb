a961c0cddd1e6019cd6aac41fbaaa22788989bd8
Mtsantos07 <mtsantos015@gmail.com>
2025-06-29 22:34:34 -0300

    Index.aspx.vb

---
+++ Index.aspx.vb
@@ -0,0 +1,10 @@
+Public Class index
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Session("Usuario") IsNot Nothing Then
+            
+        End If
+    End Sub
+
+End Class
